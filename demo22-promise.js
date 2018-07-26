5ceef6ccbfdc1e128370489d4c7f6f812420d289
郝海亮 <2298763729@qq.com>
2018-07-27 07:36:42 +0800

    add: promise-all, promise-race

--- demo22-promise.js
+++ demo22-promise.js
@@ -1,5 +1,5 @@
 {
-    // 所有图片都加载完成后, 同时显示到桌面
+	// 所有图片都加载完成后, 同时显示到桌面
 	function loadImg(src) {
 		return new Promise((resolve, reject) => {
 			let img = document.createElement('img');
@@ -24,4 +24,64 @@
 		loadImg('http://www.bmw.com.cn/content/dam/bmw/marketCN/common/HomepageTeaser/Medium/2018/calculator.jpg/_jcr_content/renditions/cq5dam.resized.img.585.low.time1518076035208.jpg'),
 		loadImg('http://www.bmw.com.cn/content/dam/bmw/marketCN/common/HomepageTeaser/Medium/2018/training.jpg/_jcr_content/renditions/cq5dam.resized.img.585.low.time1518076033884.jpg')
 	]).then(showImg);
+}
+
+{
+	function race(time) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				console.log( `${time} S - all` );
+				resolve(time);
+			}, time)
+		})
+	}
+
+	Promise.all([
+		race(2000),
+		race(1000),
+		race(3000),
+		race(4000)
+	]).then(time => {
+		console.log( ...time );
+	});
+
+}
+
+{
+	function race(time) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				console.log( `${time} S - race` );
+				resolve(time);
+			}, time)
+		})
+	}
+
+	Promise.race([
+		race(2000),
+		race(1000),
+		race(3000),
+		race(4000)
+	]).then(time => {
+		console.log( time );
+	});
+}
+
+{
+	// Promse循环
+	function race(time) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				console.log( `${time} S - all` );
+				resolve(time);
+			}, time)
+		})
+	}
+
+	let loop;
+
+	if(loop === undefined) {
+		race(2000).
+	}
+
 }
